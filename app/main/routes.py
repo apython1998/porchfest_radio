8030a34a78081ed8c88b3f7fb94b5be5ad724bb1
apython1998 <alex.python1998@gmail.com>
2019-02-07 16:47:01 -0500

    Changed main routes to use bp and to have correct imports

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,15 @@
-from app import app, db
-from flask import render_template, url_for, redirect, flash, request, jsonify
+from app import db
+from flask import render_template, url_for, redirect, flash, request, jsonify, current_app
+from flask_login import current_user, login_required
+from app.main import bp
 
 
-@app.route('/reset_db')
+@bp.route('/reset_db')
 def reset_db():
     pass
 
 
-@app.route('/')
-@app.route('/index')
+@bp.route('/')
+@bp.route('/index')
 def index():
     return render_template('index.html')
