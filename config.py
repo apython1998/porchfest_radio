1fe2a2b17c934bd79b02dd755289af0877ef7d11
apython1998 <alex.python1998@gmail.com>
2019-02-07 17:14:09 -0500

    Changed db config, testing to use TestConfig

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MONGODB_SETTINGS = {
+        'db': 'porchfest_radio',
+        'host': 'mongodb://localhost/porchfest_radio'
+    }
--- tests.py
+++ tests.py
@@ -6,3 +6,19 @@ from config import Config
 
 class TestConfig(Config):
     TESTING = True
+    # MONGODB_SETTINGS = {
+    #         'db': 'porchfest_radio_test',
+    #         'host': 'mongodb://localhost/porchfest_radio_test'
+    #     }
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    # test shit here
