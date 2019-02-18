976405341c9bd069f66a3b1c230d04b1c3f2c11f
apython1998 <alex.python1998@gmail.com>
2019-02-17 22:25:24 -0500

    Added edit user form

---
+++ app/main/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, BooleanField, SelectField, SelectMultipleField
+from wtforms.fields.html5 import DateTimeField, DateField, DateTimeLocalField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, length, URL, Optional
+from app.models import User, Artist, Location, Porch, Porchfest
+from bson.objectid import ObjectId
+from datetime import datetime
+
+
+class EditUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Submit')
+
+    def validate_email(self, email):
+        user = User.objects(email=email.data).first()
+        if user is not None and current_user.email != user.email:
+            raise ValidationError('Email already being used!')
+
+    def validate_username(self, username):
+        user = User.objects(username=username.data).first()
+        if user is not None and current_user.username != user.username:
+            raise ValidationError('Username is already being used!')
